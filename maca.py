baaf2f356a63826db9afe850aeebd0d7f9e3a373
Hearing whale songs in summer <41997163+chestnutly@users.noreply.github.com>
2022-02-09 23:16:46 +0800

    Update maca.py

--- maca.py
+++ maca.py
@@ -42,42 +42,42 @@ class Run_Main():
             ma25+=float(data[i][4])
         return round(ma25/25,2)
     def loop_run(self):
-        print("时间：   ",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        print("开始运行")
+        print("time:  ",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        print("begin")
         data=binan.get_klines("NEARUSDT","15m", 25)
         cha=self.cam5("NEARUSDT","15m",data[-6:-1])-self.cam25("NEARUSDT","15m",data)
         bianhua.append(cha)
         while True:
-            print("时间：   ",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            print("time:   ",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             #price=binan.get_klines("NEARUSDT", "15m", 25)[0][4]
             data=binan.get_klines("NEARUSDT","15m", 25)
             print("price",data[-1][4])
             cha=self.cam5("NEARUSDT","15m",data[-6:-1])-self.cam25("NEARUSDT","15m",data)
             bianhua.append(cha)
-            if cha>0:     #短时均线高于长时均线，价格被高估
+            if cha>0:     # price high
                 if bianhua[-1]<bianhua[-2]:
                     time.sleep(600)
                     data=binan.get_klines("NEARUSDT","15m", 25)
                     cha=self.cam5("NEARUSDT","15m",data[-6:-1])-self.cam25("NEARUSDT","15m",data)
                     bianhua.append(cha)
-                    if bianhua[-1]<bianhua[-2] :   #再次确认
+                    if bianhua[-1]<bianhua[-2] :   #again
                         #price=binan.get_klines("NEARUSDT", "15m", 1)[0][4]
                         sell.append(float(data[-1][4]))
-            elif cha<0:    #短时均线低于长时均线，价格被低估  
+            elif cha<0:    #price low  
                 if bianhua[-1]>bianhua[-2]:
                     time.sleep(600)
                     data=binan.get_klines("NEARUSDT","15m", 25)
                     cha=self.cam5("NEARUSDT","15m",data[-6:-1])-self.cam25("NEARUSDT","15m",data)
                     bianhua.append(cha)
-                    if bianhua[-1]>bianhua[-2] :   #再次确认
+                    if bianhua[-1]>bianhua[-2] :   #again
                         time.sleep(600)
                         #price=binan.get_klines("NEARUSDT", "15m", 1)[0][4]
                         buy.append(float(data[-1][4]))
             if len(bianhua)>5:bianhua.pop(0)
-            print("变化：",bianhua)
-            print("已经买了：",buy) 
-            print("已经卖了：",sell)
-            print("买卖的总价,买：   卖：    ",sum(buy),sum(sell)) 
+            print("change：",bianhua)
+            print("buy：",buy) 
+            print("sell：",sell)
+            print("all,buy：   sell：    ",sum(buy),sum(sell)) 
             time.sleep(600)
 if __name__ == "__main__":
     bianhua,buy,sell=[],[],[]
@@ -87,17 +87,12 @@ if __name__ == "__main__":
             instance = Run_Main()
             instance.loop_run()
         except Exception as e:
-            print("时间：   ",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            print("time：   ",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             warn_count+=1
-            print("出错原因：",e)
-            print("出错次数：",warn_count)
+            print("error：",e)
+            print("error count：",warn_count)
 
             time.sleep(600)
 
-    #try:
-    
-    #except Exception as e:
-    #    error_info = "报警：均线策略停止"
-    #    msg.dingding_warn(error_info)            
 
             
